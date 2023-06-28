06f26b41bab63153e47a5a7d38ae0f874fc73538
Zehra Kaya <72206348+itszehrakaya@users.noreply.github.com>
2023-06-28 16:14:18 +0300

    Update hanoi.py

--- hanoi-moves/hanoi.py
+++ hanoi-moves/hanoi.py
@@ -1,22 +1,19 @@
-def hanoi(n, source, auxiliary, target):
+def hanoi(n, src, ax, tgt):
     """
     Recursive function to solve Tower of Hanoi puzzle and list all the moves.
-
         n (int): Number of disks.
-        source (str): Source peg.
-        auxiliary (str): Auxiliary peg.
-        target (str): Target peg.
-
-
+        src (str): Source peg.
+        ax (str): Auxiliary peg.
+        tgt (str): Target peg.
     """
     if n > 0:
-        # Move n-1 disks from source peg to auxiliary peg
-        hanoi(n-1, source, target, auxiliary)
+        # Move n-1 disks from 'source' peg to auxiliary peg
+        hanoi(n-1, src, tgt, ax)
         
-        print(f"Move disk {n} from {source} to {target}")
+        print(f"Move disk {n} from {src} to {tgt}")
         
         # Move n-1 disks from auxiliary peg to target peg
-        hanoi(n-1, auxiliary, source, target)
+        hanoi(n-1, ax, src, tgt)
 
 
 if __name__ == "__main__":
